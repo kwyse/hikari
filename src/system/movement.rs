25d3a31cce0a766322b03a93325c0d772e8b7591
Krishan Wyse <kwysek@gmail.com>
2018-09-02 12:06:40 +0100

    Bring Position and Velocity fully into scope

--- src/system/movement.rs
+++ src/system/movement.rs
@@ -1,4 +1,4 @@
-use component::Component;
+use component::Component::{self, Position, Velocity};
 use system::System;
 
 use std::time::Duration;
@@ -7,11 +7,9 @@ pub struct MovementSystem;
 
 impl System for MovementSystem {
     fn update(&self, dependent: &mut Component, independent: &Component, delta: &Duration) {
-        if let Component::Position(pos_x, pos_y) = dependent {
-            if let Component::Velocity(vel_x, vel_y) = independent {
-                *pos_x += vel_x * (delta.as_secs() as f64);
-                *pos_y += vel_y * (delta.as_secs() as f64);
-            }
+        if let (Position(pos_x, pos_y), Velocity(vel_x, vel_y)) = (dependent, independent) {
+            *pos_x += vel_x * (delta.as_secs() as f64);
+            *pos_y += vel_y * (delta.as_secs() as f64);
         }
     }
 }
@@ -22,13 +20,13 @@ mod tests {
 
     #[test]
     fn position_is_updated() {
-        let mut pos = Component::Position(2.0, 2.0);
-        let vel = Component::Velocity(1.0, 1.0);
+        let mut pos = Position(2.0, 2.0);
+        let vel = Velocity(1.0, 1.0);
 
         let duration = Duration::new(3, 0);
         let system = MovementSystem;
         system.update(&mut pos, &vel, &duration);
 
-        assert_eq!(pos, Component::Position(5.0, 5.0));
+        assert_eq!(pos, Position(5.0, 5.0));
     }
 }
